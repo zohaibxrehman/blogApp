906f5e4c922b7a4eaf193f2166a7a73ebc8afb63
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-08 13:09:27 -0400

    Added index page

--- app.js
+++ app.js
@@ -6,3 +6,37 @@ var bodyParser = require('body-parser'),
 mongoose.connect('mongodb://localhost:27017/blogApp', { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: { type: Date, default: Date.now }
+});
+
+var Blog = mongoose.model('Blog', blogSchema);
+
+// Blog.create({
+// 	title: 'Test Blog',
+// 	image: '',
+// 	body: 'Body of the test blog'
+// });
+
+app.get('/', function(req, res) {
+	res.redirect('/blogs');
+});
+
+app.get('/blogs', function(req, res) {
+	Blog.find({}, function(err, blogs) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('index', { blogs });
+		}
+	});
+});
+
+app.listen(3000, function() {
+	console.log('The BlogApp serving on PORT 3000');
+});
