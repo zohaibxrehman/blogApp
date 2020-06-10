afa7c5e321501e2a84001faf47af1940306ab8ba
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-10 03:09:03 -0400

    Added edit and update routes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var bodyParser = require('body-parser'),
 	mongoose = require('mongoose'),
 	express = require('express'),
+	methodOverride = require('method-override'),
 	app = express();
 
 mongoose.connect('mongodb://localhost:27017/blogApp', { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(methodOverride('_method'));
 
 var blogSchema = new mongoose.Schema({
 	title: String,
@@ -60,6 +62,27 @@ app.post('/blogs', function(req, res) {
 		}
 	});
 });
+
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, blog){
+		if(err){
+			console.log(err);
+			res.redirect("/")
+		} else {
+			res.render("edit", {blog: blog});
+		}
+	});
+ });
+ 
+ app.put("/blogs/:id", function(req, res){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, blog){
+		if(err){
+			console.log(err);
+		} else {
+		  res.redirect("/blogs/" + blog._id);
+		}
+	});
+ });
 
 app.listen(3000, function() {
 	console.log('The BlogApp serving on PORT 3000');
