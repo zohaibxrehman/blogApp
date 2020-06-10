54bf48b17f681a1ece145359fe8331646b414dac
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-10 04:43:34 -0400

    Sanitized blog body from scripts

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var bodyParser = require('body-parser'),
 	mongoose = require('mongoose'),
 	express = require('express'),
+	expressSanitizer = require('express-sanitizer'),
 	methodOverride = require('method-override'),
 	app = express();
 
 mongoose.connect('mongodb://localhost:27017/blogApp', { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSanitizer());
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
@@ -54,6 +56,7 @@ app.get('/blogs/:id', function(req, res) {
 });
 
 app.post('/blogs', function(req, res) {
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.create(req.body.blog, function(err, newBlog) {
 		if (err) {
 			res.render('new');
@@ -75,6 +78,7 @@ app.get("/blogs/:id/edit", function(req, res){
  });
  
  app.put("/blogs/:id", function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, blog){
 		if(err){
 			console.log(err);
